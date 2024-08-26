9e2f7fa6a1c188f726fbfd21316795fc08f20341
gamlerd13 <gamlerd13@gmail.com>
2024-08-26 03:54:51 -0500

    chore: to pass lint vercel

--- src/app/home/home/page.tsx
+++ src/app/home/home/page.tsx
@@ -1,125 +1,125 @@
-"use client";
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
-import React from "react";
+// "use client";
+// import jsPDF from "jspdf";
+// import html2canvas from "html2canvas";
+// import React from "react";
 
-const generatePDF = async () => {
-  const element = document.getElementById("content-to-print");
-  if (!element) return;
-  const canvas = await html2canvas(element);
-  const imgData = canvas.toDataURL("image/png");
+// const generatePDF = async () => {
+//   const element = document.getElementById("content-to-print");
+//   if (!element) return;
+//   const canvas = await html2canvas(element);
+//   const imgData = canvas.toDataURL("image/png");
 
-  const pdf = new jsPDF({
-    orientation: "portrait",
-    unit: "mm",
-    format: [80, 240],
-  });
+//   const pdf = new jsPDF({
+//     orientation: "portrait",
+//     unit: "mm",
+//     format: [80, 240],
+//   });
 
-  pdf.addImage(imgData, "PNG", 0, 0);
-  pdf.save("boleta.pdf");
-};
+//   pdf.addImage(imgData, "PNG", 0, 0);
+//   pdf.save("boleta.pdf");
+// };
 
-const PrintButton = () => {
-  const pagador = "eder miraval el garcia";
-  const cantidad = "10100";
-  const total = "10200";
-  const fecha = "2 de octubre";
-  return (
-    <>
-      <div
-        id="content-to-print"
-        style={{ padding: "10px", fontSize: "12px", width: "70mm" }}
-      >
-        <div
-          style={{
-            alignItems: "center",
-            fontSize: "12px",
-            textAlign: "center",
-          }}
-        >
-          <p>GRUPO NEWTON PARAISO S.A.C</p>
-          <p>ISSAC NEWTON</p>
-          <p>ISSAC NEWTON</p>
-          <p>RUC 20610621351</p>
-          <p>
-            ASC. EL PARAISO DE JICAMARCA JR. LOS PINOS MZ 1 LT 14. LIMA,
-            LIMA-LIMA
-          </p>
-          <p>Email. juliaurs-22@hotmail.com</p>
+// const PrintButton = () => {
+//   const pagador = "eder miraval el garcia";
+//   const cantidad = "10100";
+//   const total = "10200";
+//   const fecha = "2 de octubre";
+//   return (
+//     <>
+//       <div
+//         id="content-to-print"
+//         style={{ padding: "10px", fontSize: "12px", width: "70mm" }}
+//       >
+//         <div
+//           style={{
+//             alignItems: "center",
+//             fontSize: "12px",
+//             textAlign: "center",
+//           }}
+//         >
+//           <p>GRUPO NEWTON PARAISO S.A.C</p>
+//           <p>ISSAC NEWTON</p>
+//           <p>ISSAC NEWTON</p>
+//           <p>RUC 20610621351</p>
+//           <p>
+//             ASC. EL PARAISO DE JICAMARCA JR. LOS PINOS MZ 1 LT 14. LIMA,
+//             LIMA-LIMA
+//           </p>
+//           <p>Email. juliaurs-22@hotmail.com</p>
 
-          <h1>BOLETA DE VENTA ELECTRONICA</h1>
-          <h1>BA01-00004063</h1>
-        </div>
-        <div>
-          <p>F. Emisión:</p>
-          <p>H. Emisión:</p>
-          <p>Cliente: </p>
-          <p>DNI:</p>
-          <p>Grado:</p>
-        </div>
+//           <h1>BOLETA DE VENTA ELECTRONICA</h1>
+//           <h1>BA01-00004063</h1>
+//         </div>
+//         <div>
+//           <p>F. Emisión:</p>
+//           <p>H. Emisión:</p>
+//           <p>Cliente: </p>
+//           <p>DNI:</p>
+//           <p>Grado:</p>
+//         </div>
 
-        <div>
-          <table
-          // style={{
-          //   // border: "1px 1px 1px solid",
-          //   borderBottom: "1px  solid",
-          // }}
-          >
-            <thead
-              style={{
-                borderBottom: "1px  solid",
-              }}
-            >
-              <tr
-                style={{
-                  paddingBottom: 8,
-                }}
-              >
-                <th>Cant.</th>
-                <th>Descripción</th>
-                <th>P.Unit</th>
-                <th>total</th>
-              </tr>
-            </thead>
-            <tbody
-              style={{
-                borderBottom: "1px  solid",
-              }}
-            >
-              <tr>
-                <td>1</td>
-                <td>PENSION PRIMARIA - D</td>
-                <td>170.00</td>
-                <td
-                  style={{
-                    fontWeight: "bold",
-                  }}
-                >
-                  170.00
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <p>{pagador}</p>
-        <p>Concepto: Servicio de Desarrollo</p>
-        <p>Monto: ${cantidad}</p>
-        <p>Monto total: ${total}</p>
-        <p>Fecha: {fecha}</p>
-      </div>
-      <button className="bg-rose-600 p-2" onClick={generatePDF}>
-        Imprimir Boleta
-      </button>
-    </>
-  );
-};
+//         <div>
+//           <table
+//           // style={{
+//           //   // border: "1px 1px 1px solid",
+//           //   borderBottom: "1px  solid",
+//           // }}
+//           >
+//             <thead
+//               style={{
+//                 borderBottom: "1px  solid",
+//               }}
+//             >
+//               <tr
+//                 style={{
+//                   paddingBottom: 8,
+//                 }}
+//               >
+//                 <th>Cant.</th>
+//                 <th>Descripción</th>
+//                 <th>P.Unit</th>
+//                 <th>total</th>
+//               </tr>
+//             </thead>
+//             <tbody
+//               style={{
+//                 borderBottom: "1px  solid",
+//               }}
+//             >
+//               <tr>
+//                 <td>1</td>
+//                 <td>PENSION PRIMARIA - D</td>
+//                 <td>170.00</td>
+//                 <td
+//                   style={{
+//                     fontWeight: "bold",
+//                   }}
+//                 >
+//                   170.00
+//                 </td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         </div>
+//         <p>{pagador}</p>
+//         <p>Concepto: Servicio de Desarrollo</p>
+//         <p>Monto: ${cantidad}</p>
+//         <p>Monto total: ${total}</p>
+//         <p>Fecha: {fecha}</p>
+//       </div>
+//       <button className="bg-rose-600 p-2" onClick={generatePDF}>
+//         Imprimir Boleta
+//       </button>
+//     </>
+//   );
+// };
 
-const style = {
-  contenedor: {
-    padding: "10px",
-    fontSize: "12px",
-    width: "70mm",
-  },
-};
+// const style = {
+//   contenedor: {
+//     padding: "10px",
+//     fontSize: "12px",
+//     width: "70mm",
+//   },
+// };
 
-export default PrintButton;
+// export default PrintButton;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,125 +1,125 @@
-"use client";
-// import ThermalPrinterComponent from "@/components/pdfTemplates/reportAlumnoPaymentsThermalPrinter";
-import { saveAs } from "file-saver";
+// "use client";
+// // import ThermalPrinterComponent from "@/components/pdfTemplates/reportAlumnoPaymentsThermalPrinter";
+// import { saveAs } from "file-saver";
 
-import ReactDOMServer from "react-dom/server";
-// import fs from "fs";
-import pdf from "html-pdf";
+// import ReactDOMServer from "react-dom/server";
+// // import fs from "fs";
+// import pdf from "html-pdf";
 
-import {
-  Br,
-  Cut,
-  Line,
-  Printer,
-  Text,
-  Row,
-  render,
-} from "react-thermal-printer";
+// import {
+//   Br,
+//   Cut,
+//   Line,
+//   Printer,
+//   Text,
+//   Row,
+//   render,
+// } from "react-thermal-printer";
 
-export default function Home() {
-  const handlePrint = async () => {
-    const data: Uint8Array = await render(
-      ThermalPrinterComponent({ alumno: "Eder Miraval Garcia" }),
-    );
+// export default function Home() {
+//   const handlePrint = async () => {
+//     const data: Uint8Array = await render(
+//       ThermalPrinterComponent({ alumno: "Eder Miraval Garcia" }),
+//     );
 
-    // Simula la impresión guardando la salida en un archivo
-    const blob = new Blob([data], { type: "application/octet-stream" });
-    saveAs(blob, "receipt.bin");
+//     // Simula la impresión guardando la salida en un archivo
+//     const blob = new Blob([data], { type: "application/octet-stream" });
+//     saveAs(blob, "receipt.bin");
 
-    console.log("Simulación de impresión: datos guardados en 'receipt.bin'");
-  };
+//     console.log("Simulación de impresión: datos guardados en 'receipt.bin'");
+//   };
 
-  const handlePrint2 = async () => {
-    const data: Uint8Array = await render(
-      ThermalPrinterComponent({ alumno: "Eder Miraval Garcia" }),
-    );
-    const port = await window.navigator.serial.requestPort();
-    console.log(port);
-    await port.open({ baudRate: 9600 });
-    const writer = port.writable?.getWriter();
-    // if (writer != null) {
-    //   await writer.write(data);
-    //   writer.releaseLock();
-    // }
-    if (writer != null) {
-      console.log("Writing data to the port...");
-      await writer.write(data);
-      writer.releaseLock();
-      console.log("Data written successfully!");
-    } else {
-      console.error("Writer is not available.");
-    }
+//   const handlePrint2 = async () => {
+//     const data: Uint8Array = await render(
+//       ThermalPrinterComponent({ alumno: "Eder Miraval Garcia" }),
+//     );
+//     const port = await window.navigator.serial.requestPort();
+//     console.log(port);
+//     await port.open({ baudRate: 9600 });
+//     const writer = port.writable?.getWriter();
+//     // if (writer != null) {
+//     //   await writer.write(data);
+//     //   writer.releaseLock();
+//     // }
+//     if (writer != null) {
+//       console.log("Writing data to the port...");
+//       await writer.write(data);
+//       writer.releaseLock();
+//       console.log("Data written successfully!");
+//     } else {
+//       console.error("Writer is not available.");
+//     }
 
-    // try {
-    //   const port = await window.navigator.serial.requestPort();
-    //   await port.open({ baudRate: 9600 });
-    //   const writer = port.writable?.getWriter();
-    //   if (writer) {
-    //     const simpleText = new TextEncoder().encode("Prueba de impresión\n");
-    //     await writer.write(simpleText);
-    //     writer.releaseLock();
-    //     console.log("Print command sent.");
-    //   } else {
-    //     console.error("No writer available.");
-    //   }
-    //   await port.close();
-    // } catch (error) {
-    //   console.error("Error al imprimir:", error);
-    // }
-  };
+//     // try {
+//     //   const port = await window.navigator.serial.requestPort();
+//     //   await port.open({ baudRate: 9600 });
+//     //   const writer = port.writable?.getWriter();
+//     //   if (writer) {
+//     //     const simpleText = new TextEncoder().encode("Prueba de impresión\n");
+//     //     await writer.write(simpleText);
+//     //     writer.releaseLock();
+//     //     console.log("Print command sent.");
+//     //   } else {
+//     //     console.error("No writer available.");
+//     //   }
+//     //   await port.close();
+//     // } catch (error) {
+//     //   console.error("Error al imprimir:", error);
+//     // }
+//   };
 
-  const handlePrint3 = async () => {
-    const data: Uint8Array = await render(
-      ThermalPrinterComponent({ alumno: "Eder Miraval Garcia" }),
-    );
+//   const handlePrint3 = async () => {
+//     const data: Uint8Array = await render(
+//       ThermalPrinterComponent({ alumno: "Eder Miraval Garcia" }),
+//     );
 
-    const textOutput = new TextDecoder().decode(data);
-    console.log("Simulación de impresión: datos a imprimir");
-    console.log(textOutput);
-  };
+//     const textOutput = new TextDecoder().decode(data);
+//     console.log("Simulación de impresión: datos a imprimir");
+//     console.log(textOutput);
+//   };
 
-  return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-around p-4">
-        <button className="bg-rose-500" type="button" onClick={handlePrint2}>
-          Imprimir boleta
-        </button>
-      </main>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <main className="flex min-h-screen flex-col items-center justify-around p-4">
+//         <button className="bg-rose-500" type="button" onClick={handlePrint2}>
+//           Imprimir boleta
+//         </button>
+//       </main>
+//     </>
+//   );
+// }
 
-const ThermalPrinterComponent = ({ alumno }: { alumno: string }) => {
-  return (
-    <Printer type="epson" width={42} characterSet="pc437_usa">
-      <Text>
-        <Text>
-          <Text bold={true}>{alumno}</Text>
-          <Line />
-          <Text>COLEGIO NEWTON PARAISO</Text>
-          <Text>ASOC. EL PARAISO DE JICAMARCA JR.</Text>
-          <Text>LOS PINOS MZ I LT 14 - LIMA</Text>
-          <Text>RUC 20610621351</Text>
-          <Text>juliaurs-22@hotmail.com</Text>
-          <Line />
-          <Text bold>REPORTE DE PAGOS</Text>
-          <Line />
-          <Text bold>INFORMACION DEL ESTUDIANTE</Text>
-          <Text>GRADO: Primero</Text>
-          <Text>SECCIÓN: Segundo grado</Text>
-          <Text>PADRE DE FAMILIA: </Text>
-          <Text>DNI:75003875</Text>
-          <Line />
-        </Text>
+// const ThermalPrinterComponent = ({ alumno }: { alumno: string }) => {
+//   return (
+//     <Printer type="epson" width={42} characterSet="pc437_usa">
+//       <Text>
+//         <Text>
+//           <Text bold={true}>{alumno}</Text>
+//           <Line />
+//           <Text>COLEGIO NEWTON PARAISO</Text>
+//           <Text>ASOC. EL PARAISO DE JICAMARCA JR.</Text>
+//           <Text>LOS PINOS MZ I LT 14 - LIMA</Text>
+//           <Text>RUC 20610621351</Text>
+//           <Text>juliaurs-22@hotmail.com</Text>
+//           <Line />
+//           <Text bold>REPORTE DE PAGOS</Text>
+//           <Line />
+//           <Text bold>INFORMACION DEL ESTUDIANTE</Text>
+//           <Text>GRADO: Primero</Text>
+//           <Text>SECCIÓN: Segundo grado</Text>
+//           <Text>PADRE DE FAMILIA: </Text>
+//           <Text>DNI:75003875</Text>
+//           <Line />
+//         </Text>
 
-        <Line />
+//         <Line />
 
-        <Row left={"CONCEPTO/PAGO"} center={"FECHA"} right={"MONTO S/."} />
-        <Line />
-        <Row left="Mes de julio" center={"21-10-12"} right="S/. 150.00" />
+//         <Row left={"CONCEPTO/PAGO"} center={"FECHA"} right={"MONTO S/."} />
+//         <Line />
+//         <Row left="Mes de julio" center={"21-10-12"} right="S/. 150.00" />
 
-        <Line />
-      </Text>
-    </Printer>
-  );
-};
+//         <Line />
+//       </Text>
+//     </Printer>
+//   );
+// };
